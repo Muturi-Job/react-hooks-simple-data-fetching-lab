8842e4fbc83a7e401b2a53cca67e9204ed9cbfa4
Job Muturi <muirimuturi17@gmail.com>
2023-04-22 14:54:03 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [dogImage, setDogImage] = useState('');
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data);
+        setDogImage(data.message);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  if (!isLoaded) return <p>Loading...</p>   
+  return <img src={dogImage} alt="A Random Dog"></img>
+}
+
+export default App;
